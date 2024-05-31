95d6b735c263ca92a0d4a99a6fec73a8d7853a37
Rajnish Dargan <rajnish.code30@gmail.com>
2024-05-31 11:54:15 +0530

    Removed Ckeditor ClassicEditor for POC

--- projects/common-form-elements/src/lib/dynamic-richtext/dynamic-richtext.component.ts
+++ projects/common-form-elements/src/lib/dynamic-richtext/dynamic-richtext.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 import { FieldConfigAsyncValidation, CustomFormControl } from '../common-form-config';
-import ClassicEditor from '@project-sunbird/ckeditor-build-classic';
+// import ClassicEditor from '@project-sunbird/ckeditor-build-classic';
 import * as _ from 'lodash-es';
 @Component({
   selector: 'sb-dynamic-richtext',
@@ -30,82 +30,82 @@ export class DynamicRichtextComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   ngOnInit() {
-    this.editorConfig = {
-      alignment: {
-        options: [
-          { name: 'left', className: 'text-left' },
-          { name: 'center', className: 'text-center' },
-          { name: 'right', className: 'text-right' }
-        ]
-      },
-      toolbar: ['bold', '|', 'italic', '|', 'underline', '|', 'insertTable',
-        '|', 'numberedList', '|', 'BulletedList', '|', 'fontSize', '|', 'alignment', '|'
-      ],
-      fontSize: {
-        options: [
-          'eight',
-          'ten',
-          'twelve',
-          'fourteen',
-          'sixteen',
-          'eighteen',
-          'twenty',
-          'twentytwo',
-          'twentyfour',
-          'twentysix',
-          'twentyeight',
-          'thirty',
-          'thirtysix'
-        ]
-      },
-      isReadOnly: this.disabled,
-      removePlugins: ['ImageCaption', 'mathtype', 'ChemType']
-    };
-    this.showEditor = true;
+    // this.editorConfig = {
+    //   alignment: {
+    //     options: [
+    //       { name: 'left', className: 'text-left' },
+    //       { name: 'center', className: 'text-center' },
+    //       { name: 'right', className: 'text-right' }
+    //     ]
+    //   },
+    //   toolbar: ['bold', '|', 'italic', '|', 'underline', '|', 'insertTable',
+    //     '|', 'numberedList', '|', 'BulletedList', '|', 'fontSize', '|', 'alignment', '|'
+    //   ],
+    //   fontSize: {
+    //     options: [
+    //       'eight',
+    //       'ten',
+    //       'twelve',
+    //       'fourteen',
+    //       'sixteen',
+    //       'eighteen',
+    //       'twenty',
+    //       'twentytwo',
+    //       'twentyfour',
+    //       'twentysix',
+    //       'twentyeight',
+    //       'thirty',
+    //       'thirtysix'
+    //     ]
+    //   },
+    //   isReadOnly: this.disabled,
+    //   removePlugins: ['ImageCaption', 'mathtype', 'ChemType']
+    // };
+    // this.showEditor = true;
   }
   ngAfterViewInit() {
-    if (this.visible) {
-      this.initializeEditors();
-    }
-    if (this.asyncValidation && this.asyncValidation.asyncValidatorFactory && this.formControlRef) {
-      if (this.formControlRef.asyncValidator) {
-        return;
-      }
-      this.formControlRef.setAsyncValidators(this.asyncValidation.asyncValidatorFactory(
-        this.asyncValidation.marker,
-        this.validationTrigger.nativeElement
-      ));
-    }
+    // if (this.visible) {
+    //   this.initializeEditors();
+    // }
+    // if (this.asyncValidation && this.asyncValidation.asyncValidatorFactory && this.formControlRef) {
+    //   if (this.formControlRef.asyncValidator) {
+    //     return;
+    //   }
+    //   this.formControlRef.setAsyncValidators(this.asyncValidation.asyncValidatorFactory(
+    //     this.asyncValidation.marker,
+    //     this.validationTrigger.nativeElement
+    //   ));
+    // }
   }
 
   initializeEditors() {
-    ClassicEditor.create(this.editorRef.nativeElement, {
-      extraPlugins: ['Font', 'Table'],
-      alignment : this.editorConfig.alignment,
-      toolbar: this.editorConfig.toolbar,
-      fontSize: this.editorConfig.fontSize,
-      isReadOnly: this.editorConfig.isReadOnly,
-      removePlugins: this.editorConfig.removePlugins,
-      wordCount: {
-        onUpdate: stats => {
-          this.characterCount = stats.characters;
-        },
-      }
-    })
-      .then(editor => {
-        this.editorInstance = editor;
-        editor.isReadOnly = this.disabled;
-        this.onChangeEditor(this.editorInstance);
-      })
-      .catch(error => {
-        console.error(error.stack);
-      });
+    // ClassicEditor.create(this.editorRef.nativeElement, {
+    //   extraPlugins: ['Font', 'Table'],
+    //   alignment : this.editorConfig.alignment,
+    //   toolbar: this.editorConfig.toolbar,
+    //   fontSize: this.editorConfig.fontSize,
+    //   isReadOnly: this.editorConfig.isReadOnly,
+    //   removePlugins: this.editorConfig.removePlugins,
+    //   wordCount: {
+    //     onUpdate: stats => {
+    //       this.characterCount = stats.characters;
+    //     },
+    //   }
+    // })
+    //   .then(editor => {
+    //     this.editorInstance = editor;
+    //     editor.isReadOnly = this.disabled;
+    //     this.onChangeEditor(this.editorInstance);
+    //   })
+    //   .catch(error => {
+    //     console.error(error.stack);
+    //   });
   }
   onChangeEditor(editor) {
-    editor.model.document.on('change', (eventInfo, batch) => {
-      this.formControlRef.markAsTouched();
-      this.formControlRef.richTextCharacterCount = this.characterCount;
-      this.formControlRef.patchValue(editor.getData());
-    });
+    // editor.model.document.on('change', (eventInfo, batch) => {
+    //   this.formControlRef.markAsTouched();
+    //   this.formControlRef.richTextCharacterCount = this.characterCount;
+    //   this.formControlRef.patchValue(editor.getData());
+    // });
   }
 }
